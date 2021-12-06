2b11c7e78bcad9c9200b702951a16b7e35ea2932
AP <60903271+PrivatPinguin@users.noreply.github.com>
2021-12-06 15:23:07 +0100

    excepts if poppler is not installed.
    shows user a link to download poppler

--- open_pdf.py
+++ open_pdf.py
@@ -12,7 +12,15 @@ def open_pdf_gui():
     filename = filedialog.askopenfilename(filetypes=[('PDF files', '.pdf')])
     #replace in filename "/" with "\\"
     filename = filename.replace("/", "\\")
-    pages = pdf2image.convert_from_path( filename, dpi=500, output_file="tmp.jpg" ,poppler_path='C:\\Program Files (x86)\\newPath\\poppler-0.68.0\\bin')
+    
+    #TODO ask user to isntall poppler, if installed choose path and save it to file.
+    
+    try:
+        pages = pdf2image.convert_from_path( filename, dpi=500, output_file="tmp.jpg" ,poppler_path='C:\\Program Files (x86)\\newPath\\poppler-0.68.0\\bin')
+    except:
+        print("You need to install Poppler and and it to PATH!\n\nDownlaod Poppler at https://github.com/oschwartz10612/poppler-windows/releases/")
+        input("Press Enter to exit.")
+        return
     for i, image in enumerate(pages):
         fname = 'image_'+str(i)+'.jpg'
         image.save(fname, "JPEG")
